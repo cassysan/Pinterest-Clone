8918b274446161b876e8c8a9116ca719aab2bd35
Cassy Myers <cassymyers@gmail.com>
2018-10-09 11:02:00 +0900

    create form, install bootstrap version of simple form

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,5 +5,19 @@ class PinsController < ApplicationController
   def show
   end
 
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+  end
+
+
+  private
+
+  def pin_params
+    params.require.permit(:title, :description)
+  end
 
 end
