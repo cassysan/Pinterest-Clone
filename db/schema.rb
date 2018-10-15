670c5501469c4f689b44363e624bbbac0a268e23
Cassy Myers <cassymyers@gmail.com>
2018-10-15 12:08:01 +0900

    add comment model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_13_151600) do
+ActiveRecord::Schema.define(version: 2018_10_15_030724) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pins", force: :cascade do |t|
     t.string "title"
